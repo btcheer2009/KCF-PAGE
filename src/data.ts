60fd842f0bbb4cf4fb9d389467774dc44e8effdd
btcheer2009 <btcheer2009@gmail.com>
2026-07-06 14:38:28 +0900

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -13,6 +13,10 @@ export const SAFETY_RULES: SafetyRule[] = [];
 
 export const INITIAL_ATHLETES: Athlete[] = [];
 
+export const EVENTS: KCFEvent[] = [];
+
+export const INITIAL_DONATION_RECORDS: DonationRecord[] = [];
+
 export const INITIAL_ASSOCIATION_INFO: AssociationInfo = {
   officeName: 'KCF 서울 중앙 사무국',
   address: '서울특별시 강서구 양천로 470, 지하 1층 비 106-14-1호',
